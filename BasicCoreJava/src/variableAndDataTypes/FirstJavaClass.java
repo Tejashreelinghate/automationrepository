91136f09ce0bd511d7e3e1d529d5a4c44165ef07
tej <tejashreelinghate@gmail.com>
2019-09-24 21:51:42 +0530

    commiting first java class for data variables

---
+++ BasicCoreJava/src/variableAndDataTypes/FirstJavaClass.java
@@ -0,0 +1,23 @@
+package variableAndDataTypes;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args)
+	{
+		int a=10;//For integer value
+		char b='A';//For single character
+		boolean c=true;//For boolean values
+		double d=6.89;//for decimal values
+		String name ="Tejashree";//For string values string is not a datatype
+		
+//Other Data types;
+		//for numbers
+		byte x=10;
+		short y=45;
+		long z=6796896;
+		//for decimal values
+		float f=67.8f;
+		System.out.println(a);
+	}
+
+}
